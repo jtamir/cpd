2319d753ef6b717af0b62614470d4161126a9e8d
Jon Tamir <jtamir@eecs.berkeley.edu>
2016-11-17 10:43:21 -0800

    move source files to subdir
    remove mex and matlab files

--- src/makeMex.m
+++
@@ -1,3 +0,0 @@
-mex -DMEX_COMPILE_FLAG -O CFLAGS="\$CFLAGS -std=c99 -pedantic" genUDCPDMex.c udcpd.c misc.c
-mex -DMEX_COMPILE_FLAG -O CFLAGS="\$CFLAGS -std=c99 -pedantic" genVDCPDMex.c udcpd.c vdcpd.c misc.c
-
